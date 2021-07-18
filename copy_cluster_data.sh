0dfe3e6c42e21350e0a0698cbcdc034b056ee26e
dventuri <diegoventuri@gmail.com>
2021-07-18 19:36:00 -0300

    checking if folder exists before copy

--- copy_cluster_data.sh
+++ copy_cluster_data.sh
@@ -14,6 +14,7 @@ MAIN_REMOTE_DIR=/home/dventuri/st_euler/$1
 # copy hdf5 files
 echo ""
 echo "Copying HDF5 file"
+if ! [ -d "$HOME/$1/output" ] ; then mkdir -p $HOME/$1/output ; fi
 rsync -avh --progress \
   $REMOTE:$MAIN_REMOTE_DIR/output/ns_output_ct.$(printf %09d $NUM).hdf5 \
   $HOME/$1/output
